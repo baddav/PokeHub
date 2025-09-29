cedb802e809430edb8021a3fb7d5a659aa316164
Kevin <kevinallgaier@icloud.com>
2025-09-29 22:10:45 +0200

    WIP

--- script.js
+++ script.js
@@ -5,13 +5,15 @@ document.querySelector('#search').addEventListener('click', getPokemon);
 function getPokemon(e) {
     // Hole den Wert aus dem Eingabefeld mit der ID "pokemonName"
     const name = document.querySelector('#pokemonName').value;
+    const box = document.querySelector(".pokemonBox"); /*Für enter*/
 
     // Sende eine Anfrage an die Pokémon-API, um Daten für das eingegebene Pokémon zu erhalten
     fetch(`https://pokeapi.co/api/v2/pokemon/${name}`)
         .then((response) => response.json()) // Konvertiere die Antwort in JSON
         .then((data) => {
+            box.style.display = "flex"; /*Für enter*/
             // Aktualisiere den Inhalt des Elements mit der Klasse "pokemonBox", um die Pokémon-Daten anzuzeigen
-            document.querySelector(".pokemonBox").innerHTML =  `
+            box.innerHTML =  `
             <div>
                 <!-- Zeige das Bild des Pokémon an -->
                 <img src="${data.sprites.other["official-artwork"].front_default}" alt="${data.name}">
@@ -26,8 +28,10 @@ function getPokemon(e) {
         .catch((err) => {
             // Logge einen Fehler in die Konsole, wenn das Pokémon nicht gefunden wird
             console.log("Pokemon not found", err);
+            box.style.display = "none";
         });
 
     // Verhindere das Standardverhalten des Buttons (z. B. das Neuladen der Seite)
     e.preventDefault();
 }
+
